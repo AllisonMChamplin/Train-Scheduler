c9d56101896f960abeab66ee6a328e7c4ff09006
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-05-29 20:58:35 -0500

    Initial site files and Firebase integration

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyC94u4nCRzLVGZW0mgfA-OnjEB_qkU3nb4",
+        authDomain: "allison-project-d6fe7.firebaseapp.com",
+        databaseURL: "https://allison-project-d6fe7.firebaseio.com",
+        projectId: "allison-project-d6fe7",
+        storageBucket: "allison-project-d6fe7.appspot.com",
+        messagingSenderId: "968944378898",
+        appId: "1:968944378898:web:59758a920920f828"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    // Create a variable to reference the database
+    var database = firebase.database();
+
+    
+
+});
