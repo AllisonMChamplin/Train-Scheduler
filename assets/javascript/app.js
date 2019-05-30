9abf6f222c18cc589abe766374a6e041f4a30c3f
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-05-30 13:03:28 -0500

    Fixed table display, added clock

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,14 +16,17 @@ $(document).ready(function () {
 
     // Create a variable to reference the database
     var database = firebase.database();
+    var trains = database.ref("trains");
+    // var trains = firebase.database().trains;
 
-
-
-    var test = $('.test');
-    test.html(moment().format());
-
-
-
+    // Clock function
+    var intervalId;
+    function update() {
+        $('#clock').html("Current Time: " + moment().format('hh:mm:ss a'));
+        clearInterval(intervalId);
+        intervalId = setInterval(update, 1000);
+    };
+    update();
 
 
     // 2. Button for adding trains
@@ -33,26 +36,18 @@ $(document).ready(function () {
         // Grabs user input
         var trainName = $("#train-name-input").val().trim();
         var trainDestination = $("#destination-input").val().trim();
+        var trainStart = $("#first-train-time-input").val().trim();
         var trainFrequency = $("#frequency-input").val().trim();
-        // var trainStart = moment($("#start-input").val().trim(), "MM/DD/YYYY").format("X");
 
         // Creates local "temporary" object for holding train data
         var newTrain = {
             name: trainName,
             destination: trainDestination,
+            start: trainStart,
             frequency: trainFrequency
-            // start: trainStart,
         };
-
         // Uploads train data to the database
-        database.ref().push(newTrain);
-
-        // Logs everything to console
-        console.log(newTrain.name);
-        console.log(newTrain.destination);
-        console.log(newTrain.frequency);
-        // console.log(newTrain.start);
-
+        trains.push(newTrain);
         alert("Train successfully added");
 
         // Clears all of the text-boxes
@@ -61,21 +56,22 @@ $(document).ready(function () {
         $("#first-train-time-input").val("");
         $("#frequency-input").val("");
     });
+    
 
     // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
-    database.ref().on("child_added", function (childSnapshot) {
+    trains.on("child_added", function (childSnapshot) {
         console.log(childSnapshot.val());
 
         // Store everything into a variable.
         var trainName = childSnapshot.val().name;
         var trainDestination = childSnapshot.val().destination;
+        var trainStart = childSnapshot.val().start;
         var trainFrequency = childSnapshot.val().frequency;
-        // var trainStart = childSnapshot.val().start;
 
         // train Info
-        console.log(trainName);
-        console.log(trainDestination);
-        console.log(trainFrequency);
+        // console.log(trainName);
+        // console.log(trainDestination);
+        // console.log(trainFrequency);
         // console.log(trainStart);
 
         // Prettify the train start
@@ -94,12 +90,10 @@ $(document).ready(function () {
         var newRow = $("<tr>").append(
             $("<td>").text(trainName),
             $("<td>").text(trainDestination),
+            $("<td>").text(trainStart),
             $("<td>").text(trainFrequency),
             $("<td>").text('Next arrival'),
             $("<td>").text('Minutes away')
-            // $("<td>").text(trainStartPretty),
-            // $("<td>").text(empMonths),
-            // $("<td>").text(empBilled)
         );
 
         // Append the new row to the table
@@ -107,44 +101,25 @@ $(document).ready(function () {
     });
 
 
-//     <table class="table table-sm table-hover" id='train-table'>
-//     <thead>
-//         <tr>
-//             <th scope="col">Train Name</th>
-//             <th scope="col">Destination</th>
-//             <th scope="col">Frequency (min)</th>
-//             <th scope="col">Next Arrival</th>
-//             <th scope="col">Minutes Away</th>
-//         </tr>
-//     </thead>
-//     <tbody>
-//     </tbody>
-// </table>
+    //// Calculation
+    var test = $('#testid');
+    var currentTime = moment();
 
-
-
-
-
-
-
-
-
-
-
-    // Example Time Math
-    // -----------------------------------------------------------------------------
-    // Assume train start date of January 1, 2015
-    // Assume current date is March 1, 2016
-
-    // We know that this is 15 months.
-    // Now we will create code in moment.js to confirm that any attempt we use meets this test case
-
-
-
-
-
-
-
+    // GET THE CHILD KEY!!
+    var query = firebase.database().ref("trains").orderByKey();
+    query.once("value")
+        .then(function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                var key = childSnapshot.key; // GET THE CHILD KEY!!
+                var childKey = childSnapshot.key;
+                console.log("key: ", key);
+                console.log("childKey: ", childKey); // GET THE CHILD KEY!!!!!!!!!!!!!!!
+                console.log("value????????????", childSnapshot.val().name);
+                test.html(childSnapshot.val().name);
+                // Cancel enumeration
+                // return true; // TRUE WILL STOP AFTER THE FIRST, OTHERWISE LOOPS THROUGH
+            });
+        });
 
 
 
